151e0b26a057c5450529cea53d599e8da39c610c
sapna770 <sapnakumawat303@gmail.com>
2021-04-11 12:13:12 +0530

    Add existing file

---
+++ src/robots.js
@@ -0,0 +1,51 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'anil kumawat',
+        username: 'anny',
+        email: 'anil.k@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Tarun Sharma',
+        username: 'tarun_ju',
+        email: 'tarun.ju@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'vivek badoria',
+        username: 'viv',
+        email: 'viv.bad@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'shnaz gill',
+        username: 'shana',
+        email: 'shanz@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'andy smith',
+        username: 'a_smith',
+        email: 'smith@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'john doe',
+        username: 'jhon_d',
+        email: 'john34@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'candy fill',
+        username: 'candy',
+        email: 'candy_fill@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'rhyme bauch',
+        username: 'rhymeee',
+        email: 'rhymee@gmail.com'
+    }
+
+] 
